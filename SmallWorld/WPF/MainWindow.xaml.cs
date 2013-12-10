c05e891c23867f9aafc7e14c8c86ffcd00c2d9cc
Eric Chauty <eric.chauty@gmail.com>
2013-12-11 00:05:12 +0100

    Ajout du scroll, mais DrawImage non fonctionnel

--- SmallWorld/WPF/MainWindow.xaml.cs
+++ SmallWorld/WPF/MainWindow.xaml.cs
@@ -12,8 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Rect;
-
+/*using System.Windows.Rect;
+using System.Windows.Media.Animation.AnimationClock;
+*/
 namespace WPF
 {
     /// <summary>
@@ -29,7 +30,7 @@ namespace WPF
 
         public void loadImages()
         {
-            Image img1 = new Image();
+            //Image img1 = new Image();
             // Create source
             BitmapImage myBitmapImage = new BitmapImage();
 
@@ -39,19 +40,16 @@ namespace WPF
             myBitmapImage.DecodePixelWidth = 200;
             myBitmapImage.EndInit();
             //set image source
-            img1.Source = myBitmapImage;
-            //myImage.Source = myBitmapImage;
+            //img1.Source = myBitmapImage;
+           // myImage.Source = myBitmapImage;
 
             //myImage.Source = new BitmapImage(new Uri(@"C:\Users\Eric\Documents\GitHub\Smallworld\SmallWorld\textures\terrains\plaine.png"));
         }
 
-        // Override the OnRender call to add a Background and Border to the OffSetPanel
         protected override void OnRender(DrawingContext dc)
         {
             Image img1 = new Image();
-            // Create source
             BitmapImage myBitmapImage = new BitmapImage();
-
             // BitmapImage.UriSource must be in a BeginInit/EndInit block
             myBitmapImage.BeginInit();
             myBitmapImage.UriSource = new Uri(@"C:\Users\Eric\Documents\GitHub\Smallworld\SmallWorld\textures\terrains\plaine.png");
@@ -59,7 +57,7 @@ namespace WPF
             myBitmapImage.EndInit();
             //set image source
             img1.Source = myBitmapImage;
-            dc.DrawImage(img1.Source, r1);
+            dc.DrawImage(img1.Source, rect1); 
         }
     }
 }
