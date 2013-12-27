f9967a2e9548e3d823ff44586be501f317841e89
gwandalf <gwlemoul@insa-rennes.fr>
2013-12-27 12:07:45 +0100

    vue unite

---
+++ SmallWorld/SmallWorldLib/GeneratedCode/Vue/VueUnite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallWorldLib.GeneratedCode.Vue
+{
+    class VueUnite
+    {
+    }
+}
